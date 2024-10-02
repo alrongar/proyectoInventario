188fc9fd0cd971d9d7f92ffbb817cf39a399f26d
alrongar <alejandroroncerogarrido@gmail.com>
2024-10-02 12:18:58 +0200

    first commit

---
+++ src/main/java/inventario/proyectoADRA2/App.java
@@ -0,0 +1,13 @@
+package inventario.proyectoADRA2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
