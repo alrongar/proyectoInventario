40828f8197f49c9a3fa1d32e41353ee9c788b6d8
alrongar <alejandroroncerogarrido@gmail.com>
2024-10-02 13:39:20 +0200

    creacion de ventana de registro

---
+++ src/main/java/views/SignIn.java
@@ -0,0 +1,81 @@
+package views;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class SignIn extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField userText;
+	private JTextField passwordText;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SignIn frame = new SignIn();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SignIn() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 589, 381);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Username:");
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		lblNewLabel.setBounds(134, 98, 94, 23);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setFont(new Font("Dialog", Font.BOLD, 15));
+		lblPassword.setBounds(134, 133, 94, 23);
+		contentPane.add(lblPassword);
+		
+		userText = new JTextField();
+		userText.setBounds(246, 98, 153, 23);
+		contentPane.add(userText);
+		userText.setColumns(10);
+		
+		passwordText = new JTextField();
+		passwordText.setColumns(10);
+		passwordText.setBounds(246, 136, 153, 23);
+		contentPane.add(passwordText);
+		
+		JButton signInBtn = new JButton("Sign In");
+		signInBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = userText.getText();
+				String password = passwordText.getText();
+			}
+		});
+		signInBtn.setFont(new Font("Dialog", Font.BOLD, 16));
+		signInBtn.setBounds(246, 203, 115, 41);
+		contentPane.add(signInBtn);
+	}
+
+}
